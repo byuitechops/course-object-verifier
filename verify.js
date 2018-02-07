e2d3a115f15b54caf2f367b3f97fcbb3fdf84513
Green <danverde@byui.edu>
2018-02-07 11:52:20 -0700

    added lessonFolders to settings

--- verify.js
+++ verify.js
@@ -3,14 +3,14 @@
 //const chalk = require('chalk');
 
 module.exports = (courseObj, callback) => {
-    const standardProperties = [
+   const standardProperties = [
         'logs',
         'settings',
         'info',
         'content'
     ];
 
-    const standardInfoProperties = [
+   const standardInfoProperties = [
         'originalZipPath',
         'unzippedPath',
         'processedPath',
@@ -18,50 +18,51 @@ module.exports = (courseObj, callback) => {
         'fileName'
     ];
 
-    const standardSettingsProperties = [
+   const standardSettingsProperties = [
         'debug',
-        'online',
         'readAll',
+        'online',
         'keepFiles',
-        'deleteCourse'
+        'deleteCourse',
+       'lessonFolders'
     ];
-    var error = null;
+   var error = null;
 
-    /* Check if courses top level contains the standard properties */
-    standardProperties.forEach((property) => {
-        if (!Object.keys(courseObj).includes(property)) {
-            error = new Error(`Course object missing property: ${property}`);
-        }
-    });
+   /* Check if courses top level contains the standard properties */
+   standardProperties.forEach((property) => {
+      if (!Object.keys(courseObj).includes(property)) {
+         error = new Error(`Course object missing property: ${property}`);
+      }
+   });
 
-    /* Check if Info contains the standard properties */
-    if (error == null) {
-        standardInfoProperties.forEach((property) => {
-            if (!Object.keys(courseObj.info).includes(property)) {
-                error = new Error(`Course Info missing property: ${property}`);
-            }
-        });
-    }
+   /* Check if Info contains the standard properties */
+   if (error == null) {
+      standardInfoProperties.forEach((property) => {
+         if (!Object.keys(courseObj.info).includes(property)) {
+            error = new Error(`Course Info missing property: ${property}`);
+         }
+      });
+   }
 
-    /* Check if Settings contains the standard properties */
-    if (error == null) {
-        standardSettingsProperties.forEach((property) => {
-            if (!Object.keys(courseObj.settings).includes(property)) {
-                error = new Error(`Course Settings missing property: ${property}`);
-            }
-        });
-    }
+   /* Check if Settings contains the standard properties */
+   if (error == null) {
+      standardSettingsProperties.forEach((property) => {
+         if (!Object.keys(courseObj.settings).includes(property)) {
+            error = new Error(`Course Settings missing property: ${property}`);
+         }
+      });
+   }
 
-    /* Check if object contains extra properties */
-    if (error == null) {
-        if (Object.keys(courseObj).length > standardProperties.length ||
-            Object.keys(courseObj.settings).length > standardSettingsProperties.length) {
-            error = new Error('Course object provided contains extra properties it should not have');
-        }
-    }
-    if (error == null) {
-        courseObj.message('Course Object successfully verified!');
-    }
+   /* Check if object contains extra properties */
+   if (error == null) {
+      if (Object.keys(courseObj).length > standardProperties.length ||
+         Object.keys(courseObj.settings).length > standardSettingsProperties.length) {
+         error = new Error('Course object provided contains extra properties it should not have');
+      }
+   }
+   if (error == null) {
+      courseObj.message('Course Object successfully verified!');
+   }
 
-    callback(error, courseObj);
+   callback(error, courseObj);
 };
